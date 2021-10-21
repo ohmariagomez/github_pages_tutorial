781c0f0b0456e9866dd4597ca1d45da89113a8ea
ohmariagomez <78579607+ohmariagomez@users.noreply.github.com>
2021-10-21 14:29:57 -0400

    Update maria.js

--- maria.js
+++ maria.js
@@ -8,10 +8,10 @@ function showGif(response){
     choiceResponse.innerHTML += '<img src="https://media.giphy.com/media/5tw3kCvxXW1XrE6Dxj/giphy.gif" />';
     choiceResponse.style.display = "block";
   } else if (response = "Pasta") {
-    choiceResponse.innerHTML += '<img src="https://media.giphy.com/media/5tw3kCvxXW1XrE6Dxj/giphy.gif" />';
+    choiceResponse.innerHTML += '<img src="https://media.giphy.com/media/gzE15pfW1jxkI/giphy.gif" />';
     choiceResponse.style.display = "block";
   } else {
-    choiceResponse.innerHTML += '<img src="https://media.giphy.com/media/5tw3kCvxXW1XrE6Dxj/giphy.gif" />';
+    choiceResponse.innerHTML += '<img src="https://media.giphy.com/media/Vm2qtmm2nxVP0ZJ96n/giphy.gif" />';
     choiceResponse.style.display = "block";
   }
 }
