5bf9b306b9e58b2a7b26ce1648555835d18e9e55
ohmariagomez <78579607+ohmariagomez@users.noreply.github.com>
2021-10-21 14:27:57 -0400

    Update maria.js

--- maria.js
+++ maria.js
@@ -1,1 +1,17 @@
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceResponse = document.getElementById("choiceResponse");
 
+function showGif(response){
+  if (response = "Fenway"){
+    choiceResponse.innerHTML += '<img src="https://media.giphy.com/media/5tw3kCvxXW1XrE6Dxj/giphy.gif" />';
+    choiceResponse.style.display = "block";
+  } else if (response = "Pasta") {
+    img = "";
+    choiceResponse.style.display = "block";
+  } else {
+    img = "";
+    choiceResponse.style.display = "block";
+  }
+}
